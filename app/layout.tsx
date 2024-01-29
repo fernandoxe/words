965f2b110c01c7dd91f3caa92fa6f6457145d94c
Fernando Parra <fernandoxe@gmail.com>
2024-01-29 18:44:27 +0100

    chore: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,88 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
+import { Nunito } from 'next/font/google';
+import { config, gtm } from '@/config';
+import { headers } from 'next/headers';
+import { Metadata, Viewport } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Nunito({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Words',
-  description: 'A words wordle',
+export const generateMetadata = (): Metadata => {
+  const headerList = headers();
+  const host = headerList.get('host') || '';
+
+  const appConfig = config[host];
+  const title = appConfig.appTitle;
+  const description = appConfig.appDescription;
+  const publicPath = appConfig.publicPath;
+
+  return {
+    metadataBase: new URL(appConfig.url),
+    title,
+    description,
+    keywords: appConfig.metaKeywords,
+    openGraph: {
+      title,
+      description,
+      type: 'website',
+      locale: 'en_US',
+      url: appConfig.url,
+      siteName: title,
+      images: [
+        {
+          url: `${publicPath}/og-image-192x192.png`,
+          width: 192,
+          height: 192,
+          alt: title,
+        },
+      ],
+    },
+    robots: 'index, follow',
+    icons: {
+      icon: [
+        {url: `${publicPath}/favicon-32x32.png`},
+        {url: `${publicPath}/favicon-16x16.png`},
+        {url: `${publicPath}/favicon.ico`},
+        {rel: 'mask-icon', url: `${publicPath}/safari-pinned-tab.svg`, color: '#0f172a'},
+      ],
+      apple: `${publicPath}/apple-touch-icon.png`,
+    },
+    manifest: `${publicPath}/manifest.json`,
+    category: 'Games',
+  };
 };
+
+export const viewport: Viewport = {
+  themeColor: '#0f172a',
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const headerList = headers();
+  const host = headerList.get('host') || '';
+  const gtmId = gtm[host]; console.log('gtm', gtm);
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <script src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`} />
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtmId}');
+            `
+          }
+        } />
+      </head>
+      <body className={`${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
