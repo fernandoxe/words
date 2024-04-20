d5e23c42df018e0915ea53baa98ed71d969429b0
Fernando Parra <fernandoxe@gmail.com>
2024-04-20 02:57:42 -0300

    chore: utc date

--- services/date.ts
+++ services/date.ts
@@ -2,5 +2,6 @@ export const formatDate = (date: string) => {
   return new Intl.DateTimeFormat('en-US', {
     month: 'numeric',
     day: 'numeric',
+    timeZone: 'UTC',
   }).format(new Date(date));
 };
